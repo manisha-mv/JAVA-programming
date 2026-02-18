26fa265cac306bbf7e7611749ec24d34c95721f4
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-18 23:10:34 +0530

    p24

---
+++ Basics/p24.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class p24{
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        long registration = scanner.nextLong();
+        long broadcast = scanner.nextLong();
+        long sponsor = scanner.nextLong();
+        long prize = scanner.nextLong();
+        long rent = scanner.nextLong();
+        long advertising = scanner.nextLong();
+        
+        
+        long remainingFund = registration
+                             + broadcast
+                             + sponsor
+                             - prize
+                             - rent
+                             - advertising;
+        
+        
+        System.out.println("Remaining Tournament Fund = " + remainingFund);
+        
+        scanner.close();
+    }
+}
