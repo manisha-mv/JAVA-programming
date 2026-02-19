b6b2b8721294710a659e508a44b77d33b5f31a6b
manisha-mv <manishamanivannan1505@gmail.com>
2026-02-20 01:27:14 +0530

    p13

---
+++ operators/p13.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class p13 {
+    public static void  main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if((a & b)!=0){
+            System.out.println("Appliance Active");
+        }
+        else{
+            System.out.println("Inactive");
+        }
+        sc.close();
+}
+}
