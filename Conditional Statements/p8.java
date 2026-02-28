105eb34d5b6aec607cc33d445dc8833b9fafc51e
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-01 00:25:41 +0530

    p8

--- Conditional Statements/p8.java
+++ Conditional Statements/p8.java
@@ -1,94 +1,90 @@
+
 import java.util.Scanner;
 
-public class p8 {
-    public static void main(String[] args) {
+public class SchlorshipEligibiltyp8 {
+    public static void main(String[]args){
 
         Scanner sc = new Scanner(System.in);
 
-        String location = sc.next();
-        int squareFootage = sc.nextInt();
+        String Tier = sc.next();
+        int sq_ft = sc.nextInt();
         int age = sc.nextInt();
-        int amenityScore = sc.nextInt();
+        int amenity = sc.nextInt();
 
-        double basePricePerSqFt = 0;
-        double ageAdjustment = 0;
-        double amenityBonus = 0;
+        double base_price = 0.0;
+        int age_adjust = 0;
+        double amenity_bonus = 0.0;
 
+        double base_value = 0.0;
         
-        if (location.equals("Prime")) {
-            basePricePerSqFt = 400;
-        } 
-        else if (location.equals("Urban")) {
-            basePricePerSqFt = 300;
-        } 
-        else if (location.equals("Suburban")) {
-            basePricePerSqFt = 180;
-        } 
-        else {
-            basePricePerSqFt = 100;  
+
+        if(Tier.equals("Prime")){
+            base_price = 400;
+        }
+        else if(Tier.equals("Urban")){
+            base_price = 300;
+        }
+        else if(Tier.equals("Suburban")){
+            base_price = 180;
+        }
+        else if(Tier.equals("Rural")){
+            base_price = 100;
         }
 
-      
-        if (age >= 0 && age <= 5) {
-            ageAdjustment = 0;
-        } 
-        else if (age <= 10) {
-            ageAdjustment = -5;
-        } 
-        else if (age <= 15) {
-            ageAdjustment = -10;
-        } 
-        else if (age <= 30) {
-            ageAdjustment = -15;
-        } 
-        else {
-            ageAdjustment = -25;
+       if (age >= 0 && age <= 5) {
+            age_adjust = 0;
+        } else if (age >= 6 && age <= 15) {
+            age_adjust = -5;
+        } else if (age >= 16 && age <= 30) {
+            age_adjust = -15;
+        } else {
+            age_adjust = -25;
         }
 
-    
-        double baseValue = squareFootage * basePricePerSqFt; 
-        double adjustedValue = baseValue * (1 + ageAdjustment / 100);
+         base_value = sq_ft*base_price;
 
-      
-        if (amenityScore >= 80) {
-            amenityBonus = adjustedValue * 0.10;
-        } 
-        else if (amenityScore >= 60) {
-            amenityBonus = adjustedValue * 0.05;
-        } 
-        else if (amenityScore >= 40) {
-            amenityBonus = adjustedValue * 0.02;
-        } 
+        if(amenity>=80 && amenity<=100){
+            amenity_bonus = base_value*0.10;
+        }
+        else if(amenity>=60 && amenity<=79){
+            amenity_bonus = base_value*0.05;
+        }
+        else if(amenity>=40 && amenity<=59){
+            amenity_bonus = base_value*0.02;
+        }
         else {
-            amenityBonus = 0;
+            amenity_bonus = 0;
         }
 
-        double finalValue = adjustedValue + amenityBonus;
+        double adjust_value = base_value*(1+ age_adjust/100.0);
+        double fin = adjust_value+amenity_bonus;
 
-        
-        String marketCategory;
+        String market  = " ";
 
-        if ((location.equals("Prime") || location.equals("Urban")) && age < 15) {
-            marketCategory = "Hot";
-        } 
-        else if (location.equals("Suburban")) {
-            marketCategory = "Stable";
-        } 
-        else {
-            marketCategory = "Slow";
-        }
 
-       
-        System.out.println("Location Tier: " + location);
-        System.out.println("Square Footage: " + squareFootage + " sq ft");
-        System.out.println("Property Age: " + age + " years");
-        System.out.println("Amenity Score: " + amenityScore);
-        System.out.println("Base Price Per Sq Ft: $" + basePricePerSqFt);
-        System.out.println("Age Adjustment: " + ageAdjustment + "%");
-        System.out.println("Amenity Bonus: $" + amenityBonus);
-        System.out.println("Estimated Property Value: $" + finalValue);
-        System.out.println("Market Category: " + marketCategory);
+    if ((Tier.equals("Prime") || Tier.equals("Urban")) && age < 15) {
+        market = "Hot";
+    } 
+    else if (Tier.equals("Suburban")) {
+        market = "Stable";
+    } 
+    else {
+        market = "Slow";
+    }
+
+    System.out.println("Location Tier: " + Tier);
+    System.out.println("Square Footage: " + sq_ft + " sq ft");
+    System.out.println("Property Age: " + age + " years");
+    System.out.println("Amenity Score: " + amenity);
+    System.out.println("Base Price Per Sq Ft: $" + base_price);
+    System.out.println("Age Adjustment: " + age_adjust + "%");
+    System.out.printf("Amenity Bonus: $%.1f%n" , amenity_bonus);
+    System.out.printf("Estimated Property Value: $%.1f%n" ,fin);
+    System.out.println("Market Category: " + market);
 
         sc.close();
+
+
+
     }
 }
