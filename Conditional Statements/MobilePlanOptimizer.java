b24d3807e4c75d41452b9d85643ae5dede4c64d8
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-04 12:13:50 +0530

    p25

--- Conditional Statements/MobilePlanOptimizer.java
+++ Conditional Statements/MobilePlanOptimizer.java
@@ -1,117 +1,136 @@
 import java.util.Scanner;
 
-public class MobilePlanOptimizer {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-       
+public class mobileDataPlan_p25{
+    public static void main(String[]args){
+        Scanner sc = new Scanner (System.in);
         double dataGB = sc.nextDouble();
-        int callMinutes = sc.nextInt();
-        sc.nextLine();
-        String internationalUsage = sc.nextLine();
-        String deviceType = sc.nextLine();
+        int call_min = sc.nextInt();
+        String Internet_usage = sc.next();
+        String device_type = sc.next();
 
-        String recommendedPlan = "";
-        double baseCost = 0;
-        double dataOverage = 0;
-        double callOverage = 0;
-        double internationalFee = 0;
-        double deviceFee = 0;
-        String category = "";
-
-       
-        if (dataGB <= 2 && callMinutes <= 500) {
-            recommendedPlan = "Basic 2GB";
-            baseCost = 25;
-            category = "Budget";
-
-            if (dataGB > 2) {
-                dataOverage = (dataGB - 2) * 10;
-            }
-
-        } else if (dataGB <= 5 && callMinutes <= 1000) {
-            recommendedPlan = "Standard 5GB";
-            baseCost = 40;
-            category = "Standard";
-
-            if (dataGB > 5) {
-                dataOverage = (dataGB - 5) * 10;
-            }
-
-        } else if (dataGB <= 30 && callMinutes <= 2500) {
-            recommendedPlan = "Premium Unlimited";
-            baseCost = 70;
-            category = "Premium";
-
-        } else {
-            recommendedPlan = "Unlimited Max";
-            baseCost = 90;
-            category = "Unlimited";
-        }
-
-        if (recommendedPlan.equals("Basic 2GB")) {
-            if (callMinutes > 500) {
-                callOverage = (callMinutes - 500) * 0.10;
-            }
-        } else if (recommendedPlan.equals("Standard 5GB")) {
-            if (callMinutes > 1000) {
-                callOverage = (callMinutes - 1000) * 0.10;
-            }
-        }
-
-       
-        if (internationalUsage.equals("None")) {
-            internationalFee = 0;
-        } else if (internationalUsage.equals("Light")) {
-            internationalFee = 5;
-        } else if (internationalUsage.equals("Moderate")) {
-            internationalFee = 15;
-        } else if (internationalUsage.equals("Heavy")) {
-            internationalFee = 30;
-        }
-
+        String Recom_plan = "";
+        String plan_cate = "";
+        int base_cost = 0;
         
-        if (deviceType.equals("Basic")) {
-            deviceFee = 0;
-        } else if (deviceType.equals("Smartphone")) {
-            deviceFee = 10;
-        } else if (deviceType.equals("Tablet")) {
-            deviceFee = 10;
-        } else if (deviceType.equals("Hotspot")) {
-            deviceFee = 20;
-        }
+        double data_over = 0.0;
+        double  call_over = 0.0;
+        double inter_fee = 0.0;
+        double device_fee = 0.0;
+        String higher_plan = "";
+        int higher_base_cost = 0;
 
-        
-        double totalMonthlyCost = baseCost + dataOverage + callOverage + internationalFee + deviceFee;
 
-       
-        double potentialSavings = 0;
-
-        if (recommendedPlan.equals("Standard 5GB") && dataGB > 5) {
-           
-            double premiumCost = 70 + internationalFee + deviceFee;
-            potentialSavings = totalMonthlyCost - premiumCost;
-            if (potentialSavings < 0) {
-                potentialSavings = 0;
+        if(dataGB<=2.0){
+            if(call_min<=500){
+                if(Internet_usage.equals("None") && (device_type.equals("Basic"))){
+                    Recom_plan = "Basic 2GB";
+                    plan_cate = "Budget";
+                    base_cost = 25;
+                }
             }
         }
+        else if(dataGB>2.0 && dataGB<=6.0){
+            Recom_plan = "Standard 5GB";
+            plan_cate = "Standard";
+            base_cost = 40;
+        }
+        else if(dataGB>30.0){
+            Recom_plan = "Unlimited Max";
+            plan_cate = "Unlimited";
+            base_cost =90;
+        }
 
-      
+        if(Internet_usage.equals("Heavy")){
+            Recom_plan ="Unlimited Max";
+        }
+        else if(device_type.equals("Hotspot")){
+            Recom_plan = "Unlimited Max";
+        }
+        else if(Internet_usage.equals("Moderate")){
+            Recom_plan = "Premium Unlimited";
+        }
+
+        if(Recom_plan.equals("Basic 2GB")){
+            if(dataGB>2){
+                if(call_min>500){
+                    call_over = (call_min-500)*10.0; 
+                }
+                data_over = (dataGB-2)*10;
+            }
+            plan_cate = "Budget";
+        }
+        else if(Recom_plan.equals("Standard 5GB")){
+            if(dataGB>5){
+                if(call_min>1000){
+                    call_over = (call_min-1000)*0.05;
+                }
+                data_over = (dataGB-5)*10;
+            }
+            plan_cate = "Standard";
+        }
+        else if(Recom_plan.equals("Unlimited Max") || (Recom_plan.equals("Premium Unlimited"))){
+            data_over = 0.0;
+            plan_cate = "Unlimited";
+        }
+        else{
+            data_over = 0.0;
+            plan_cate = "Premium";
+        }
+
+        if(Internet_usage.equals("None")){
+            inter_fee = 0.0;
+        }
+        else if(Internet_usage.equals("Light")){
+            inter_fee = 5.0;
+        }
+        else if(Internet_usage.equals("Moderate")){
+            inter_fee = 15.0;
+        }
+        else if(Internet_usage.equals("Heavy")){
+            inter_fee = 30.0;
+        }
+
+        if(device_type.equals("Basic")){
+            device_fee = 0.0;
+        }
+        if(device_type.equals("Smartphone")){
+            device_fee = 10.0;
+        }
+        if(device_type.equals("Tablet")){
+            device_fee = 10.0;
+        }
+        if(device_type.equals("Hotspot")){
+            device_fee = 20.0;
+        }
+        double total_cost = base_cost+data_over+call_over
+                            +inter_fee+device_fee;
+
+            // if(Recom_plan.equals("Basic 2GB")){
+            //     higher_plan = "Standard";
+            //     higher_base_cost = 40;
+            // }
+            // else if(Recom_plan.equals("Standard 5GB")){
+            //     higher_plan = "Premium Unlimited";
+            //     higher_base_cost = 70;
+            // }
+            // else if(Recom_plan.equals("Premium Unlimited")){
+            //     higher_plan = "Unlimited Max";
+            //     higher_base_cost = 90;
+            // }
+            // else{
+            //     higher_plan = "";
+            // }
         System.out.println("Data Usage: " + dataGB + " GB");
-        System.out.println("Call Minutes: " + callMinutes + " minutes");
-        System.out.println("International Usage: " + internationalUsage);
-        System.out.println("Device Type: " + deviceType);
-        System.out.println("Recommended Plan: " + recommendedPlan);
-        System.out.println("Base Plan Cost: $" + baseCost);
-        System.out.println("Data Overage: $" + dataOverage);
-        System.out.println("Call Overage: $" + callOverage);
-        System.out.println("International Fee: $" + internationalFee);
-        System.out.println("Device Fee: $" + deviceFee);
-        System.out.println("Total Monthly Cost: $" + totalMonthlyCost);
-        System.out.println("Potential Savings: $" + potentialSavings);
-        System.out.println("Plan Category: " + category);
+        System.out.println("Call Minutes: " + call_min + " minutes");
+        System.out.println("International Usage: " + Internet_usage);
+        System.out.println("Device Type: " + device_type);
+        System.out.println("Base plan Cost: " + base_cost);
+        System.out.println("Data Overage: $" + data_over);
+        System.out.println("Call Overage: $" + call_over);
+        System.out.println("International Fee: $" + inter_fee);
+        System.out.println("Device Fee: $" + device_fee);
+        System.out.println("Total Monthly Cost: $" + total_cost);
+        System.out.println("Plan Category: " + plan_cate);
 
         sc.close();
     }
