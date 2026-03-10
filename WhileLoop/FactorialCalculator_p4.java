e9388b7c10d5337ec9dba37221c9f0b2282cb26a
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-10 16:14:12 +0530

    p4

---
+++ WhileLoop/FactorialCalculator_p4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FactorialCalculator_p4 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int result = 1;
+        int counter = 1;
+        int num = sc.nextInt();
+        
+
+        while(counter<=num){
+            result*= counter;
+            counter++;
+
+        }
+    System.out.println("Factorial Of "+num +" = "+result);
+    }
+}
