896ca34913ab02feb4264300c8d1a8aa009145fc
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-10 11:23:01 +0530

    p20

--- Looping/for_loop/BankTransactionProcessor.java
+++ Looping/for_loop/BankTransactionProcessor.java
@@ -1,58 +1,53 @@
 import java.util.Scanner;
 
-public class BankTransactionProcessor {
-    public static void main(String[] args) {
-
+public class BankProcessor_p20 {
+    public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-
-        double totalDeposits = 0.0;
-        double totalWithdrawals = 0.0;
-        double totalTransfers = 0.0;
-        double totalPayments = 0.0;
-
-        for(int i = 1; i <= n; i++){
-
+        double total_deposits = 0;
+        double total_Withdrawal = 0;
+        double total_transfer = 0;
+        double total_payment = 0;
+        double net_balance = 0;
+        
+        for(int i = 1;i<=n;i++){
             String type = sc.next();
-            double amount = sc.nextDouble();
+            double amt = sc.nextDouble();
 
-            String category = "";
+            String category  = "";
 
+            System.out.println();
+            System.out.println("Transaction "+i+" : "+type);
+            System.out.println("Amount : $"+amt);
             if(type.equals("Deposit")){
                 category = "Credit";
-                totalDeposits += amount;
+                total_deposits +=amt;
             }
-            else if(type.equals("Withdrawal")){
+            else if(type.equals("Withdrwal") || (type.equals("Tranfer") || (type.equals("Payment")))){
                 category = "Debit";
-                totalWithdrawals += amount;
+            }
+            System.out.println("Category : "+category);
+            System.out.println();
+            if(type.equals("Withdrawal")){
+                total_Withdrawal+=amt;
             }
             else if(type.equals("Transfer")){
-                category = "Debit";
-                totalTransfers += amount;
+                total_transfer+=amt;
             }
             else if(type.equals("Payment")){
-                category = "Debit";
-                totalPayments += amount;
+                total_payment+=amt;
             }
-
-            System.out.println("Transaction " + i + ": " + type);
-            System.out.println("Amount: $" + amount);
-            System.out.println("Category: " + category);
-
-            if(i < n){
-                System.out.println();
-            }
+            net_balance = total_deposits-(total_Withdrawal+total_transfer+total_payment);
         }
-
-        double netBalance = totalDeposits - (totalWithdrawals + totalTransfers + totalPayments);
-
         System.out.println();
-        System.out.println("Total Transactions: " + n);
-        System.out.println("Total Deposits: $" + totalDeposits);
-        System.out.println("Total Withdrawals: $" + totalWithdrawals);
-        System.out.println("Total Transfers: $" + totalTransfers);
-        System.out.println("Total Payments: $" + totalPayments);
-        System.out.println("Net Balance Change: $" + netBalance);
+        System.out.println("Total Transactions : "+n);
+        System.out.println("Total Deposits : $"+total_deposits);
+        System.out.println("Total Withdrawals : $"+total_Withdrawal);
+        System.out.println("Total Transfers : $"+total_transfer);
+        System.out.println("Total Payments : $"+total_payment);
+        System.out.println("Net Balance Change : $"+net_balance);
+        System.out.println();
+        sc.close();
     }
 }
