a88804f7a5a4478222f994665f51a55573d6cf69
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-10 16:32:08 +0530

    p5

---
+++ Reverse_p5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Reverse_p5 {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = 0;
+        int rev = 0;
+       int  num = n;
+        while(n!=0){
+            d = n%10;
+            rev = rev*10+d;
+            n = n/10;
+
+            // int num = n;
+        }
+        System.out.println("Original : "+num);
+        System.out.println("Reversed : "+rev);
+        sc.close();
+    }
+}
