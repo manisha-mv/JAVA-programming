b40db1348477d048caf2b1f0511d641928a70396
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-13 23:56:01 +0530

    p20

---
+++ WhileLoop/Strong_number_p20.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Strong_number_p20 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int rem;
+        int fact = 1;
+        int sum = 0 ;
+        int original = n;
+
+        while(n>0){
+            rem = n%10;
+            for(int i = rem;i<=1;i--){
+                fact = fact*i;
+                
+            }
+            sum = sum+fact;
+            n= n/10;
+        }
+        if (n == original){
+            System.out.print(" %d Strong NUmber",original);
+        } 
+        else{
+          System.out.println("%d is not a storng number",original);
+        }       
+    }
+}
