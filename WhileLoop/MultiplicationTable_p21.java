8f7c3448b79b1b8dbbd7176fefd95265729f0d3d
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-15 22:41:10 +0530

    p21

---
+++ WhileLoop/MultiplicationTable_p21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MultiplicationTable_p21 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int range = sc.nextInt();
+
+        int i = 1;
+
+        System.out.println("Multiplication table of " + number + ":");
+
+        do {
+            System.out.println(number + " x " + i + " = " + (number * i));
+            i++;
+        } while (i <= range);
+    }
+}
