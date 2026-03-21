11ec3a6dc172a8279e78aa28e5618fe0ebe3c66b
manisha-mv <manishamanivannan1505@gmail.com>
2026-03-21 23:03:03 +0530

    p25

---
+++ WhileLoop/Pattern_p25.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pattern_p25 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        
+        int i = 1;
+
+        do{
+            int j = 1;
+        do{
+            System.out.print(" "+i);
+            j++;
+        }while(j<=i);
+        System.out.println();
+        i++;
+
+    }while(i<=n);
+    }
+}
